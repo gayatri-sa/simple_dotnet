28520fa4c18c560b766e632356faed495b757de9
Gayatri Ajith <gayatri@schogini.com>
2021-02-02 13:24:16 +0000

    Changed the sample MySQL.sql code

--- SQL/MySQL.sql
+++ SQL/MySQL.sql
@@ -1,13 +1,13 @@
 
 CREATE DATABASE IF NOT EXISTS ajaxsamples;
 USE ajaxsamples;
-CREATE TABLE `ajaxsamples`.`customers` (
+CREATE TABLE IF NOT EXISTS `ajaxsamples`.`customers` (
   `CustomerId` INT NOT NULL AUTO_INCREMENT,
   `Name` VARCHAR(100) NOT NULL,
   `Country` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`CustomerId`));
 
-INSERT INTO Customers (Name, Country)
+REPLACE INTO Customers (Name, Country)
 SELECT 'John Hammond', 'United States'
 UNION ALL
 SELECT 'Mudassar Khan', 'India'
