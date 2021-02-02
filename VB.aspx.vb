e679451fb7b7aec763eed3a91971a3a5c50280be
Gayatri Ajith <gayatri@schogini.com>
2021-02-02 06:53:24 +0000

    Simple ASP.NET web project with MySQL

---
+++ VB.aspx.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Data
+Imports System.Configuration
+Imports MySql.Data.MySqlClient
+Partial Class VB
+    Inherits System.Web.UI.Page
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If Not Me.IsPostBack Then
+            Dim constr As String = ConfigurationManager.ConnectionStrings("constr").ConnectionString
+            Using con As New MySqlConnection(constr)
+                Using cmd As New MySqlCommand("SELECT * FROM Customers")
+                    Using sda As New MySqlDataAdapter()
+                        cmd.Connection = con
+                        sda.SelectCommand = cmd
+                        Using dt As New DataTable()
+                            sda.Fill(dt)
+                            GridView1.DataSource = dt
+                            GridView1.DataBind()
+                        End Using
+                    End Using
+                End Using
+            End Using
+        End If
+    End Sub
+End Class
